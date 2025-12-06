dab3d7ce13d4301037778efd5fda6e131957d830
Muhammad-Talha103 <talhagp908@gmail.com>
2025-12-06 16:57:24 +0500

    updates

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,7 +17,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/about`,
+      url: `${baseUrl}/help`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
