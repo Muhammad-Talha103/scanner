ad49245dfed2da5cf2232ee3d700e129140bcb33
Muhammad-Talha103 <talhagp908@gmail.com>
2025-12-13 13:29:44 +0500

    update code

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -62,12 +62,12 @@ export async function POST(req: Request) {
     if (session.payment_intent) {
       paymentIntent = (await stripe.paymentIntents.retrieve(
         session.payment_intent as string,
-        { expand: ["charges.data.payment_method"] }
+        { expand: ["charges"] }
       )) as Stripe.PaymentIntent & { charges?: Stripe.ApiList<Stripe.Charge> };
     }
 
     const charge = paymentIntent?.charges?.data?.[0];
-    const paymentMethod = charge?.payment_method as Stripe.PaymentMethod | null;
+    const paymentMethod = charge?.payment_method_details;
     const billingAddress =
       charge?.billing_details?.address || session.customer_details?.address;
 
