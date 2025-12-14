2958025217c01777c02d723e29ad178bcf88219a
Muhammad-Talha103 <talhagp908@gmail.com>
2025-12-14 16:22:41 +0500

    updates

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,6 +6,7 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  token: "skcuVbXrIvwREVUFG1Ll2foc2XSe81NpREOWHQLLOeBhd0X3JuSkIW9raOxXfEzLvwfAUfhqoX9itD63H1MVzt4mXERI9Vk22csaiR52D5ac0IRUCQqH4QFL4VndyPMXJSwttWpq4MS1ExsjParQualNKzxwqUU2ANLR81S2zkDoBbodKNRr", 
+  useCdn: true, 
+  token: process.env.SANITY_TOKEN,
 })
+
